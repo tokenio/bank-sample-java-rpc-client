25aba95332045f0a94734fdc29ad23c33bf81d13
DanielS-Token <daniel.shenfield@token.io>
2021-07-21 17:27:51 +0100

    Added a GetBalance request with explination

--- src/io/token/exmple/client/Main.java
+++ src/io/token/exmple/client/Main.java
@@ -7,12 +7,16 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslProvider;
 import io.token.proto.bankapi.AccountServiceGrpc;
 import io.token.proto.bankapi.AccountServiceGrpc.AccountServiceBlockingStub;
+import io.token.proto.bankapi.Bankapi.GetBalanceRequest;
+import io.token.proto.bankapi.Bankapi.GetBalanceResponse;
 import io.token.proto.bankapi.Bankapi.HealthCheckRequest;
 import io.token.proto.bankapi.Bankapi.HealthCheckResponse;
 import io.token.proto.bankapi.HealthCheckServiceGrpc;
 import io.token.proto.bankapi.HealthCheckServiceGrpc.HealthCheckServiceBlockingStub;
 import io.token.proto.bankapi.TransferServiceGrpc;
 import io.token.proto.bankapi.TransferServiceGrpc.TransferServiceBlockingStub;
+import io.token.proto.common.account.AccountProtos.AccountFeatures;
+import io.token.proto.common.account.AccountProtos.BankAccount;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -59,6 +63,49 @@ public class Main {
             System.out.println("--> OUT: " + request);
             HealthCheckResponse response = health.healthCheck(request);
             System.out.println("<-- IN: " + response);
+
+
+            //If the HealthCheck was successful then we can move on to testing GetBalance
+
+            /* Example of the account object coop currently use (This account object originates from the banks implementation of the Bank SDK (when creating an Access Consent) )
+              "account": {
+                "accountFeatures": {
+                  "supportsInformation": true,
+                  "supportsReceivePayment": true,
+                  "supportsSendPayment": true
+                },
+                "custom": {
+                  "bankId": "coop-sit2",
+                  "payload": "2cnHuGDFpNqyhCwSCnnpKLW/CBOSXsnAVOMe6qKkaM8=|m:3ikLDpahw1Rx11RDMhWQdDF8jpq4:5zKtXEAq"
+                }
+              }
+             */
+            String consentId = "ta:9tJNYhJ1a4NxPhtgsCqWcLiVGpXJTYjyBUZcr3bNwu9A:5zKtXEAq"; //This is the token id (usually Token will automatically populate this value), it depends on how the Bank-Integration SDK is used by the bank as to whether this is required (it's likely it is not required).
+            /* A GetBalanceRequest has 2 possible fields: consentId and account
+                The consentId is a string whereas the account is a BankAccount object, you can see an example of this object above and you can set the same values using the builder:
+                BankAccount.newBuilder()
+                //Set the values for the BankAccount object here
+                .build()
+
+               Protobuf for reference: https://developer.token.io/bank-integration/pbdoc/io_token_proto_bankapi.html#GetBalanceRequest
+             */
+            GetBalanceRequest getBalanceRequest = GetBalanceRequest.newBuilder()
+                    .setConsentId(consentId)
+                    .setAccount(BankAccount.newBuilder()
+                            .setAccountFeatures(AccountFeatures.newBuilder()
+                                    .setSupportsInformation(true)
+                                    .setSupportsSendPayment(true)
+                                    .setSupportsReceivePayment(true)
+                                    .build())
+                            .setCustom(BankAccount.Custom.newBuilder()
+                                    .setBankId("coop-sit2")
+                                    .setPayload("2cnHuGDFpNqyhCwSCnnpKLW/CBOSXsnAVOMe6qKkaM8=|m:3ikLDpahw1Rx11RDMhWQdDF8jpq4:5zKtXEAq")
+                                    .build())
+                            .build())
+                    .build();
+
+            GetBalanceResponse getBalanceResponse = accounts.getBalance(getBalanceRequest);
+            System.out.println("GetBalance returned: " + getBalanceResponse.getAvailable().getValue() + getBalanceResponse.getAvailable().getCurrency());
 
         } finally {
             channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
